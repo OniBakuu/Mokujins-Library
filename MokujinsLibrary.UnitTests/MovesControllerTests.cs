16cdd1ea54fd37a0f72d072713334d21d27bcb2a
OniBakuu <doogie971@gmail.com>
2022-06-22 23:08:42 -0400

    Added Test for GetMoveAsync
    checks for expected Move

--- MokujinsLibrary.UnitTests/MovesControllerTests.cs
+++ MokujinsLibrary.UnitTests/MovesControllerTests.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MokujinsLibrary.Controllers;
+using MokujinsLibrary.Dtos;
 using MokujinsLibrary.Entities;
 using Moq;
 using Xunit;
@@ -11,14 +14,15 @@ namespace MokujinsLibrary.UnitTests {};
 
 public class MovesControllerTests
 {
+    private readonly Mock<IMoveRepo> repoStub = new();
+    private readonly Mock<ILogger<MovesController>> loggerStub = new();
+
     [Fact]
     public async Task GetMoveAsync_WithUnexistingMove_ReturnsNotFound()
     {
         //Arrange
-        var repoStub = new Mock<IMoveRepo>();
         repoStub.Setup(repo => repo.GetMoveAsync(It.IsAny<string>(), It.IsAny<string>()))
             .ReturnsAsync((Move)null);
-        var loggerStub = new Mock<ILogger<MovesController>>();
 
         var controller = new MovesController(repoStub.Object, loggerStub.Object);
 
@@ -26,6 +30,39 @@ public class MovesControllerTests
         var result = await controller.GetMove("dragunov", "d+2");
 
         //Assert
-        Assert.IsType<NotFoundResult>(result.Result);
+        //Assert.IsType<NotFoundResult>(result.Result);
+        result.Result.Should().BeOfType<NotFoundResult>();
+    }
+
+    [Fact]
+    public async Task GetMoveAsync_WithExistingMove_ReturnsExpectedMove()
+    {
+        //Arrange
+        var expectedMove = CreateTestMove();
+        
+        repoStub.Setup(repo => repo.GetMoveAsync(It.IsAny<string>(), It.IsAny<string>()))
+            .ReturnsAsync(expectedMove);
+        var controller = new MovesController(repoStub.Object, loggerStub.Object);
+
+        //Act
+        var result = await controller.GetMove("", "");
+        
+        //Assert
+        result.Value.Should().BeEquivalentTo(expectedMove, options => options.ComparingByMembers<Move>());
+    }
+
+    private Move CreateTestMove()
+    {
+        return new()
+        {
+            character = "TestChar",
+            moveName = "TestMoveName",
+            input = "TestInputString",
+            damage = "TestDamageVal",
+            hitLevel = "TestHitLevel",
+            framesStartup = "TestFrames",
+            framesOnBlock = "TestFrames",
+            notes = " "
+        };
     }
 }
